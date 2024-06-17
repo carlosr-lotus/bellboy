c2c5e9a0c2b26c43b5d096aa9984d0ad64ed81ff
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-06-17 12:41:52 -0300

    UPD login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,7 @@
-'use client'
-// import Image from "next/image";
-import { Fragment, useState } from "react";
-
-// Components
-import Sidebar from "@/components/Sidebar";
-import DashboardHeader from "@/components/DashboardHeader";
-import DashSectionTitle from "@/components/DashSectionTitle";
-import ServiceBox from "@/components/ServiceBox";
-
-// Types
-import { ServiceProps } from "@/components/ServiceBox";
+"use client";
 
 import styles from "./page.module.css";
 
 export default function Home() {
-
-  const servicesData = [
-    { id: 1, icon: '', name: 'Netflix', color: '#E50914', status: true },
-    { id: 2, icon: '', name: 'Max', color: '#941DE8', status: true },
-    { id: 3, icon: '', name: 'Prime', color: '#1399FF', status: false }
-  ]
-
-  const [services, setServices] = useState<ServiceProps[]>(servicesData)
-
-  return (
-    <main className={styles.appContainer}>
-      <Sidebar />
-
-      <div className={styles.dashboardContainer}>
-        <DashboardHeader />
-        <DashSectionTitle />
-        
-        <div className={styles.services}>
-          {services &&
-            services.map((service) => (
-              <Fragment key={service.id}>
-                <ServiceBox 
-                  id={service.id}
-                  icon=""
-                  name={service.name}
-                  color={service.color}
-                  status={service.status}
-                />
-              </Fragment>
-            ))
-          }
-        </div>
-      </div>
-    </main>
-  );
+  return <main className={styles.appContainer}>LOGIN PAGE</main>;
 }
