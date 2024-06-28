8fdbde2103e6e175d79dd574569108a107e9ff2d
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-06-28 00:05:27 -0300

    UPD change port

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 export function getApi(ctx?: any): AxiosInstance {
     const api = axios.create({
-        baseURL: "http://localhost:4000"
+        baseURL: "http://localhost:3000"
     });
 
     return api;
