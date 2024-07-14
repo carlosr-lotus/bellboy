2111b4df41b2d3d8de7ee5c57770e1424121ee17
Carlos Lotus <carlosr_juniorpro@protonmail.com>
2024-07-14 00:02:18 -0300

    ADD: default icon profile

--- src/components/ProfileIcon.tsx
+++ src/components/ProfileIcon.tsx
@@ -1,8 +1,11 @@
+import { FaUser } from "react-icons/fa6";
+
 import styles from '@/styles/components/ProfileIcon.module.css'
 
 export default function ProfileIcon(): JSX.Element {
     return (
         <div className={styles.container}>
+            <FaUser size={15} />
         </div>
     )
 }
